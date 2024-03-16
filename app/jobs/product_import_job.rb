0521937d74aeca0f9d20911fd75e239b97ddcc92
Laurie Roy <laurieroy.dev@gmail.com>
2024-03-16 14:18:18 -0500

    Update product_import_job to use turbo

--- app/jobs/product_import_job.rb
+++ app/jobs/product_import_job.rb
@@ -3,15 +3,37 @@ class ProductImportJob < ApplicationJob
 
   def perform(link, stack_id)
     stack = Stack.find(stack_id)
+    imported_product = nil
 
     existing_product = Product.find_by(link: link)
 
     if existing_product
-      existing_product.stacks << stack
+      imported_product = existing_product.stacks << stack
     else
       product = Product.new(link: link)
       product.stacks << stack
-      ProductParser::Amazon.save_to_product(link, product)
+      imported_product = ProductParser::Amazon.save_to_product(link, product)
     end
+
+    broadcast_turbo_stream(stack, imported_product)
+  end
+
+  private
+
+  def broadcast_turbo_stream(stack, product)
+    Turbo::StreamsChannel.broadcast_replace_to(
+      stack, 
+      target: "product_list_#{stack.id}",
+      partial: "stacks/product_list",
+      locals: { stack: stack }
+    )
+
+    Turbo::StreamsChannel.broadcast_replace_to(
+      stack, 
+      target: "products_count_#{stack.id}",
+      partial: "stacks/stack_product_count",
+      locals: { stack: stack }
+    )
   end
 end
+
